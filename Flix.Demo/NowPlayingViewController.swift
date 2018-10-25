565d5bbac8b4d1e14370c9cf7dcae111483acaec
laurasalazarg <43196006+laurasalazarg@users.noreply.github.com>
2018-10-24 21:26:29 -0500

    updates :rocket:

--- Flix.Demo/NowPlayingViewController.swift
+++ Flix.Demo/NowPlayingViewController.swift
@@ -10,17 +10,12 @@ import UIKit
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
-    
-
-    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
-        
-        
 
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
     let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -36,26 +31,20 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 for movie in movies {
                     let title = movie["title"] as! String
                     print(title)
-                    
                 }
             }
         }
         task.resume()
-        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
         return cell
     }
-    
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
